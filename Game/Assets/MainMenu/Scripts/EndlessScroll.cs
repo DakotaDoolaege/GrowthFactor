6e5b02ddca46308a3bde05c3a61baf02712bfad0
Arnoldo Marin <arnoldo.emarin@gmail.com>
2019-10-04 20:15:51 -0600

    Add menu and connect to game scene

---
+++ Game/Assets/MainMenu/Scripts/EndlessScroll.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndlessScroll : MonoBehaviour { 
+
+    public float scrollingSpeed; // speed of scrolling background (negative to go right)
+    Vector2 initialPosition; // begining position
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialPosition = transform.position; // get current position at start
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float newPos = Mathf.Repeat(Time.time * scrollingSpeed, 20); // calculate new position
+        transform.position = initialPosition - Vector2.right * newPos; // move to that postion to the right
+    }
+}
