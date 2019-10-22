a812366030f34d3faea1951dac6404e6ff267a6b
Barimir <37191387+Barimir@users.noreply.github.com>
2019-10-21 20:20:29 -0600

    Add main menu music and sound effects

--- Game/Assets/MainMenu/Scripts/MainMenu.cs
+++ Game/Assets/MainMenu/Scripts/MainMenu.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement; // change scenes
 
 /// <summary>
-/// Deals with every fucntion related to the main menu.
+/// Deals with every function related to the main menu.
 /// </summary>
 public class MainMenu : MonoBehaviour
 {
@@ -13,6 +13,19 @@ public class MainMenu : MonoBehaviour
     /// </summary>
     public void PlayGame()
     {
+        // call to coroutine
+        StartCoroutine("Wait");
+        
+    }
+
+    /// <summary>
+    /// Corountine will load the next scene after waiting for half a second
+    /// </summary>
+    /// <returns>none</returns>
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(0.5f); // wait 1/2 a second
+
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); // get the scene next in the queue after current 
     }
 }
