c5e028c7b5b88b3dd484d4e453c71e10650a9a89
Dakota Doolaege <dakotadoolaege@gmail.com>
2019-10-28 21:29:12 -0600

    Create player class to store scores and locations
    We can now store a list of player stations that contain positions and
    scores, This is used to update the scores on the player stations on the
    save screen. Some functions can be converted to use this instead of the
    old list of player locations (vector3)

--- Game/Assets/EndLevelScript.cs
+++ Game/Assets/EndLevelScript.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement; // change scenes
+using TMPro;
 
 
 public class EndLevelScript : MonoBehaviour
 {
-        public GameObject PlayerPanel;
+    public GameObject PlayerPanel;
     public void ResumeGame()
     {
         GameVariables.Paused = false;
@@ -28,18 +29,17 @@ public class EndLevelScript : MonoBehaviour
         gameObject.SetActive(false);
         CreatePlayerPanels();
         gameObject.SetActive(true);
-        GameObject.Find("SaveScoresOverlay"); //.SetActive(true);
-        Debug.Log("IT IS: " + gameObject.name);  
         GameVariables.Paused = false;
         //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - SceneManager.GetActiveScene().buildIndex); // get the scene next in the queue after current 
     }
     public void MainMenu()
     {
-        Debug.Log("PARENT: " + gameObject.transform.parent.name);
         gameObject.SetActive(false);
         gameObject.transform.parent.gameObject.SetActive(false);
-        
+
         GameVariables.Paused = false;
+        GameVariables.Players = new List<Vector3>();
+        GameVariables.PlayerList = new List<GameVariables.PlayerStation>();
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - SceneManager.GetActiveScene().buildIndex); // get the scene next in the queue after current 
     }
 
@@ -47,31 +47,33 @@ public class EndLevelScript : MonoBehaviour
     {
 
         GameObject overlay = gameObject.transform.gameObject;
-        Debug.Log("Object name: " + overlay.name);
-        overlay.transform.Translate(new Vector3(0, 0, 0)); 
+        overlay.transform.Translate(new Vector3(0, 0, 0));
         for (int i = 0; i < GameVariables.Players.Count; i++)
-            {
-            Debug.Log("Creating keyboard");
-                Vector3 startPosition = GameVariables.Players[i];
+        {
+            Vector3 startPosition = GameVariables.Players[i];
             Debug.Log(startPosition);
-                if(startPosition.y > 1500)
+            if (startPosition.y > 1500)
             {
-                Debug.Log("TOP!");
                 startPosition.y = startPosition.y - 50;
                 GameObject Keyboard = Instantiate(PlayerPanel, startPosition, Quaternion.AngleAxis(-180, Vector3.forward)) as GameObject;
                 Keyboard.transform.SetParent(gameObject.transform, false);
-
+                TextMeshProUGUI Score = Keyboard.transform.GetChild(2).GetComponent<TMPro.TextMeshProUGUI>();
+                Score.text = GameVariables.PlayerList[i].GetScore();
             }
-            else if(startPosition.x < 500)
+            else if (startPosition.x < 500)
             {
                 GameObject Keyboard = Instantiate(PlayerPanel, startPosition, Quaternion.AngleAxis(-90, Vector3.forward)) as GameObject;
                 Keyboard.transform.SetParent(gameObject.transform, false);
+                TextMeshProUGUI Score = Keyboard.transform.GetChild(2).GetComponent<TMPro.TextMeshProUGUI>();
+                Score.text = GameVariables.PlayerList[i].GetScore();
 
             }
-            else if(startPosition.x > 3000)
+            else if (startPosition.x > 3000)
             {
                 GameObject Keyboard = Instantiate(PlayerPanel, startPosition, Quaternion.AngleAxis(90, Vector3.forward)) as GameObject;
                 Keyboard.transform.SetParent(gameObject.transform, false);
+                TextMeshProUGUI Score = Keyboard.transform.GetChild(2).GetComponent<TMPro.TextMeshProUGUI>();
+                Score.text = GameVariables.PlayerList[i].GetScore();
 
             }
             else
@@ -79,11 +81,14 @@ public class EndLevelScript : MonoBehaviour
                 startPosition.y = startPosition.y + 50;
                 GameObject Keyboard = Instantiate(PlayerPanel, startPosition, Quaternion.identity) as GameObject;
                 Keyboard.transform.SetParent(gameObject.transform, false);
+                TextMeshProUGUI Score = Keyboard.transform.GetChild(2).GetComponent<TMPro.TextMeshProUGUI>();
+                Score.text = GameVariables.PlayerList[i].GetScore();
 
             }
+            
 
             Debug.Log("Setting parent to:" + gameObject.name);
-            }
+        }
     }
 
 }
