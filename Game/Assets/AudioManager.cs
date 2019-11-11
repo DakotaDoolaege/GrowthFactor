e37527d96d9a04c41f7c5c3ec8e1446f75cd4eb4
Barimir <37191387+Barimir@users.noreply.github.com>
2019-11-11 13:30:39 -0700

    create audio manager that persists through scenes

--- Game/Assets/AudioManager.cs
+++ Game/Assets/AudioManager.cs
@@ -1,27 +1,98 @@
 ﻿using UnityEngine.Audio;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class AudioManager : MonoBehaviour
 {
-    public bool isMuted = false;
+    
+    // FindObjectOfType<AudioManager>().Play("SoundName")
+    public bool isMuted = false; // game muted
 
+    public Sound[] listOfSounds;
+
+    // avoid creating new audio manager in every scene loaded
+    public static AudioManager audioManagerInstance;
+
+    /// <summary>
+    /// Gets all the audio sources at the start of the game
+    /// </summary>
+    private void Awake()
+    {
+        // if there is no audio manager in scene
+        if (audioManagerInstance == null)
+        {
+            AudioManager audioManager = this;
+            audioManagerInstance = audioManager;
+        }
+        else // already have audio manager
+        {
+            Destroy(gameObject);
+        }
+
+        // audio manager persists across scenes
+        DontDestroyOnLoad(gameObject);        
+
+        //set volume, clip, and loop for each sound
+        foreach (Sound sound in listOfSounds)
+        {
+            sound.soundSource = gameObject.AddComponent<AudioSource>();
+            sound.soundSource.volume = sound.soundVolume;
+            sound.soundSource.clip = sound.soundClip;
+            sound.soundSource.loop = sound.soundLoop;
+        }      
+    }
+
+    /// <summary>
+    /// Play Game theme
+    /// </summary>
+    private void Start()
+    {
+        Play("GameTheme");
+    }
+
+    /// <summary>
+    /// Looks for the sound that matches the name provided and if it finds it, it will play it
+    /// </summary>
+    /// <param name="specificSoundName">name of sound that the game wants to play</param>
+    public void Play(string specificSoundName)
+    {
+        Sound givenSound = null;
+
+        // find the sound in the sounds array that is equal to to the name provided
+        foreach (Sound sound in listOfSounds)
+        {
+            if (sound.soundName == specificSoundName)
+            {
+                givenSound = sound;
+            }
+        }
+
+        if (givenSound == null) // if sound was not found, it won't give null pointer exception
+        {
+            Debug.LogWarning(specificSoundName + " sound was not found");
+            return;
+        }
+
+        givenSound.soundSource.Play(); // play the sound provided
+    }
 
     /// <summary>
     /// Mutes or Unmutes game sound
     /// </summary>
     public void Mute()
     {
-        if (this.isMuted == false)
+        // checks if audio manager is already muted; if it is, unmutes; if not, mutes
+        if (audioManagerInstance.isMuted == false)
         {
             AudioListener.volume = 0;
-            this.isMuted = true;
+            audioManagerInstance.isMuted = true;
         }
 
         else 
         {
-            AudioListener.volume = 0.7f;
-            this.isMuted = false;
+            AudioListener.volume = 1f;
+            audioManagerInstance.isMuted = false;
 
         }
     }
---
+++ Game/Assets/Sound.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+[System.Serializable]
+public class Sound
+{
+    public string soundName;
+    public AudioClip soundClip;
+
+    [Range(0f, 1f)]
+    public float soundVolume;
+
+    public bool soundLoop;
+
+    [HideInInspector]
+    public AudioSource soundSource;
+}
