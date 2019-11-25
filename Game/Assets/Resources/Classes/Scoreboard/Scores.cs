bc01d4e74f794713e4395ac3f9a6eefc2b272512
samuel <samuelfneumann@gmail.com>
2019-11-25 04:52:52 -0700

    Modify Scores class to update upon DB update

--- Game/Assets/Resources/Classes/Scoreboard/Scores.cs
+++ Game/Assets/Resources/Classes/Scoreboard/Scores.cs
@@ -2,10 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Class <c>Scores</c> holds the top 10 current scores in the database.
+/// </summary>
 public static class Scores
 {
     static Database Connection { get; set; } = GameObject.FindGameObjectWithTag("Database").GetComponent<Database>();
-    static IList AllScores { get; set; } = Connection.TopSingle();
+    // static IList AllScores { get; set; } = Connection.TopSingle();
+    static IList AllScores { get => Connection.TopSingle(); }
 
     public static IList<string> GetScore(int place)
     {
