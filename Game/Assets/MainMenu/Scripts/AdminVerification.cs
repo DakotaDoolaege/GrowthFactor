13d2fb28eae37371fafd72ad6d2c6514413e6573
Dakota Doolaege <dakotadoolaege@gmail.com>
2019-11-24 20:30:16 -0700

    Add login ui to the admin overlay and ui for removing bad names(Scores)

---
+++ Game/Assets/MainMenu/Scripts/AdminVerification.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdminVerification : MonoBehaviour
+{
+	public GameObject SoundButton;
+	public GameObject RemoveName;
+	public GameObject LoginUI;
+
+	//add remaining authenticated admin options here
+
+	private static Database DB;
+    // Start is called before the first frame update
+    void Start()
+    {
+		DB = GameObject.Find("DBScript").GetComponent<Database>();
+    }
+
+	/// <summary>
+	/// Logs the admin in and shows the full menu
+	/// </summary>
+	/// <param name="Password"></param>
+	public void Login(InputField Password)
+	{
+		if(DB.Authenticate("Dakota",Password.text) == true)
+		{
+			Debug.Log("User authenticated");
+			SoundButton.SetActive(true);
+			RemoveName.SetActive(true);
+			LoginUI.SetActive(false);
+			Password.text = "";
+			//Password.transform.parent.gameObject.SetActive(false);
+		}
+	}
+
+	public void Logout()
+	{
+		SoundButton.SetActive(false);
+		RemoveName.SetActive(false);
+		LoginUI.SetActive(true);
+	}
+
+	/// <summary>
+	/// Removes a score from the database
+	/// </summary>
+	/// <param name="Name"></param>
+	public void DeleteName(InputField Name)
+	{
+		DB.RemoveScore(Name.text);
+		Debug.Log("Removing: " + Name.text);
+		Name.text = "";
+	}
+
+
+
+
+}
